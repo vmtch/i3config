1c2cdf1cd06bdceb6b6282f6ae874001230a85a1
vmtch <ktakahiro2001@gmail.com>
2025-01-06 17:26:30 +0000

    changed monitor's formation and add exec to --no-startup-id

--- slideshow.sh
+++ slideshow.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-while [ 1 ]; do
-    feh --randomize --bg-tile ~/Pictures/wallpaper
-    sleep 10;
-done
+#while [ 1 ]; do
+#    feh --randomize --bg-tile ~/Pictures/wallpaper
+#    sleep 10;
+#done
+feh --randomize --bg-tile ~/Pictures/wallpaper
